9af4b9136b857683e115c970fe3d15109fc6aafd
phuoc <21013187@st.phenikaa-uni.edu.vn>
2024-01-16 08:10:19 +0700

    add graphic shadow

---
+++ java_btl/src/main/java/graphic/ShadowType.java
@@ -0,0 +1,9 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package graphic;
+
+public enum ShadowType {
+    CENTER, TOP_RIGHT, TOP_LEFT, BOT_RIGHT, BOT_LEFT, BOT, TOP
+}
